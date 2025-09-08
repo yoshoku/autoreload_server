4d26e199ad253218ed7c8119ebbac43ef94af5b4
yoshoku <yoshoku@outlook.com>
2025-09-08 13:13:21 +0900

    chore: setup project

---
+++ lib/autoreload_web_server.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "autoreload_web_server/version"
+
+module AutoreloadWebServer
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/autoreload_web_server_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe AutoreloadWebServer do
+  it "has a version number" do
+    expect(AutoreloadWebServer::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
