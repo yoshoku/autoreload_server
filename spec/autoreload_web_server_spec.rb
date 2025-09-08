a1edd81b133695bb49feb8ea76127ff2a582f3f6
yoshoku <yoshoku@outlook.com>
2025-09-08 13:24:43 +0900

    refactor: auto-correct RSpec/BeNil

--- spec/autoreload_web_server_spec.rb
+++ spec/autoreload_web_server_spec.rb
@@ -2,10 +2,6 @@
 
 RSpec.describe AutoreloadWebServer do
   it "has a version number" do
-    expect(AutoreloadWebServer::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
+    expect(AutoreloadWebServer::VERSION).not_to be_nil
   end
 end
