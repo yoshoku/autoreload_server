cd092c2c6e57da32d73b77b5eaaabb9f484fb026
yoshoku <yoshoku@outlook.com>
2025-09-08 17:02:25 +0900

    feat: implement Client class

--- lib/autoreload_web_server.rb
+++ lib/autoreload_web_server.rb
@@ -2,4 +2,5 @@
 
 require_relative 'autoreload_web_server/version'
 require_relative 'autoreload_web_server/server'
+require_relative 'autoreload_web_server/client'
 require_relative 'autoreload_web_server/watcher'
--- lib/autoreload_web_server/client.rb
+++ lib/autoreload_web_server/client.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+require 'optparse'
+require_relative 'server'
+
 module AutoreloadWebServer
   class Client
     def initialize
@@ -12,6 +15,72 @@ module AutoreloadWebServer
       }
     end
 
-    def run(args); end
+    def run(args)
+      parse_options(args)
+      validate_options
+      start_server
+      resecue => e
+      puts "[autoreload-web-server] Error: #{e.message}"
+      exit 1
+    end
+
+    private
+
+    def parse_options(args) # rubocop:disable Metrics/AbcSize
+      parser = OptionParser.new do |opts|
+        opts.banner = 'Usage: autoreload-web-server [options] [directory] [port]'
+        opts.version = AutoreloadWebServer::VERSION
+
+        opts.on('-w', '--watch PATTERN', "File pattern to watch (default: #{@opts[:watch]})") do |pattern|
+          @opts[:watch] = pattern
+        end
+
+        opts.on('-h', '--help', 'Show this help message') do
+          puts opts
+          exit
+        end
+
+        opts.on('-v', '--version', 'Show version') do
+          puts AutoreloadWebServer::VERSION
+          exit
+        end
+      end
+
+      remaining = parser.parse!(args)
+
+      if remaining.size == 1
+        @opts[:directory] = remaining[0]
+      elsif remaining.size == 2
+        @opts[:directory] = remaining[0]
+        @opts[:port] = remaining[1].to_i
+      elsif remaining.size > 2
+        warn 'Too many arguments.'
+        puts parser
+        exit 1
+      end
+    end
+
+    def validate_options
+      unless Dir.exist?(@opts[:directory])
+        warn "Directory '#{@opts[:directory]}' does not exist"
+        exit 1
+      end
+
+      return if @opts[:port].between?(1, 65_535)
+
+      warn 'Port must be between 1 and 65535'
+      exit 1
+    end
+
+    def start_server
+      server = Server.new(@opts)
+
+      trap('INT') do
+        puts '[autoreload-web-server] Shutting down...'
+        exit
+      end
+
+      server.start
+    end
   end
 end
